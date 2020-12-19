13b87e94ef3cf78cee3f9fd978c0167f43e5413f
ammvsu <dilshodbek2k17@yandex.ru>
2020-12-19 21:00:26 +0300

    first commit

---
+++ script.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+cd /home/ammvsu/
+mkdir laba1
+cd /home/ammvsu/laba1
+cp ~/.bash_logout /home/ammvsu/laba1
+mv .bash_logout result.bash_logout.bak
+cp ~/.bash_logout /home/ammvsu/laba1
+ mv .bash_logout first.bash_logout
+
